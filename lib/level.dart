15f48938c3b80c7861463eda0229ce9e25e017f4
Jp Siegel <jpsiegel8@gmail.com>
2022-02-13 12:36:18 -0500

    Centered the tile map in the screen.

--- lib/level.dart
+++ lib/level.dart
@@ -1,34 +1,60 @@
 import 'package:flame/components.dart';
+import 'package:github_game/github_game.dart';
+import 'dart:ui';
+import 'package:tiled/tiled.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'player.dart';
 
-class Level extends PositionComponent {
+class Level extends PositionComponent with HasGameRef<GitHubGame> {
   late final Player player;
-  late final String mapPath;
   late final Vector2 tileSize;
+  late final TiledComponent tileMapComponent;
 
-  late final TiledComponent tileMap;
+  late final TiledMap _tileMap;
+  late final String _mapPath;
+  bool _loaded = false;
 
-  bool loaded = false;
-
-  Level(this.player, this.mapPath, this.tileSize);
+  Level(this.player, this._mapPath, this.tileSize);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    tileMapComponent = await TiledComponent.load(_mapPath, tileSize);
+    _tileMap = tileMapComponent.tileMap.map;
 
-    tileMap = await TiledComponent.load(mapPath, tileSize);
-    loaded = true;
+    _loaded = true;
 
-    add(tileMap);
+    add(tileMapComponent);
+
+    _resize(gameRef.canvasSize);
   }
 
   @override
-  void onGameResize(canvasSize) {
-    super.onGameResize(canvasSize);
+  void onGameResize(Vector2 gameSize) {
+    super.onGameResize(gameSize);
 
-    if (loaded) {
+    if (_loaded) {
       // Center the canvas
+      _resize(gameSize);
     }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+  }
+
+  void _resize(Vector2 canvasSize) {
+    position.x = _getMiddle(canvasSize.x, _tileMap.width * tileSize.x);
+    position.y = _getMiddle(canvasSize.y, _tileMap.height * tileSize.y);
+  }
+
+  double _getMiddle(double canvasSize, double width) {
+    return (canvasSize / 2) - (width / 2);
   }
 }
