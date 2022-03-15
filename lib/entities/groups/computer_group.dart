4c163b4db4fedf94850cc3d2c29f041e83f221eb
Jp Siegel <jpsiegel8@gmail.com>
2022-03-15 13:59:55 -0400

    Github API added.

--- lib/entities/groups/computer_group.dart
+++ lib/entities/groups/computer_group.dart
@@ -1,19 +1,15 @@
 import 'package:github_game/entities/entity_group.dart';
 import 'package:github_game/entities/individuals/computer.dart';
 import 'dart:collection';
-import 'package:git/git.dart';
 import 'package:github_game/mixins/has_level_ref.dart';
-import 'package:path/path.dart' as p;
 
 class ComputerGroup extends EntityGroup<Computer> with HasLevelRef {
   late final HashSet<String> _repoPaths;
   late final String _repoDirectoryPath;
-  late final HashMap<String, GitDir> _repos;
 
   ComputerGroup(
       String id, List<dynamic> entityJsons, Map<String, dynamic>? properties)
       : _repoPaths = HashSet(),
-        _repos = HashMap(),
         super(id, entityJsons, properties) {
     if (properties?["repos"] != null) {
       List<dynamic> repos = properties!["repos"];
@@ -31,7 +27,7 @@ class ComputerGroup extends EntityGroup<Computer> with HasLevelRef {
     _repoDirectoryPath = "assets/assets/levels/${level.id}/repos";
 
     for (String repoPath in _repoPaths) {
-      await GitDir.fromExisting("${p.current}/$_repoDirectoryPath/$repoPath");
+      print("$_repoDirectoryPath/$repoPath");
     }
   }
 }
